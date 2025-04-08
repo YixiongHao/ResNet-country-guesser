81b8dc906b29598fdf79e614745728bdd73d7f0c
Kepler Boyce <kboyce7@login-ice-3.pace.gatech.edu>
2025-04-08 15:21:58 -0400

    Kaggle dataset models and graphs

--- plot.sh
+++ plot.sh
@@ -2,10 +2,24 @@
 depths=("18" "34" "50" "101" "152")
 residuals=("True" "False")
 
+# Check if dir is provided
+if [ -z "$1" ]; then
+    echo "Missing history directory argument"
+    echo "Usage: $0 <directory>"
+    exit 1
+fi
+
+dir=$(realpath "$1")
+
+if [ ! -d "$dir" ]; then
+    echo "Invalid directory: $dir"
+    exit 1
+fi
+
 # Loop over depth and residual settings and submit the job with sbatch
 for depth in "${depths[@]}"; do
     for residual in "${residuals[@]}"; do
-        cmd="python plot.py /home/hice1/yhao96/ResNet-country-guesser/config+history/resnet${depth}_residual=${residual}_transfer=False_history.json"
+        cmd="python plot.py $dir/resnet${depth}_residual=${residual}_transfer=False_history.json"
         echo "Executing: $cmd"
         eval $cmd
     done
